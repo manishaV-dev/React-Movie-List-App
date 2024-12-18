be2d93b13ae680d0593fec672d34b03587f23ea6
Manisha varma <manishavkapex1@gmail.com>
2024-12-18 13:16:58 +0530

    add routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Favorite from "./pages/Favorite";
 
 function App() {
   return (
     <>
-      <Home />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorite" element={<Favorite />} />
+        </Routes>
+      </main>
     </>
   );
 }
---
+++ src/pages/Favorite.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Favorite() {
+  return (
+    <>
+    
+    <div className="favorite-empty">
+        <h2>No Favorite Movies Yet</h2>
+        <p>Start adding movies to your favorite and they will appear here</p>
+    </div>
+    </>
+  )
+}
+
+export default Favorite
