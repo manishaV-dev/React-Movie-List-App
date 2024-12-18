83ef73efbcabd5d360bcc84a19b2822b94436bf8
Manisha varma <manishavkapex1@gmail.com>
2024-12-18 13:00:38 +0530

    add some functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     {
       id: 1,
@@ -19,9 +22,9 @@ function Home() {
     },
   ];
 
-
-  function handleSearch(e){
-    e.preventDefault()
+  function handleSearch(e) {
+    e.preventDefault();
+    alert(searchQuery);
   }
 
   return (
@@ -32,8 +35,25 @@ function Home() {
             type="text"
             placeholder="search for movie..."
             className="search-input"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
           />
+          <button type="submit" className="search-btn">
+            Search
+          </button>
         </form>
+
+        {/* <div className="movies-grid">
+          {movies.map(
+            (movie) =>
+                // define search function-- first change movie to lowercase as it is string, then use startsWith method and give searchQuery, now when we type in search box if there movie available it shows that
+              movie.title.toLowerCase().startsWith(searchQuery) && (
+                <MovieCard movie={movie} key={movie.id} />
+              )
+
+              do not use this search because we are using API for this
+          )}
+        </div> */}
 
         <div className="movies-grid">
           {movies.map((movie) => (
